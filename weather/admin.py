4477e9db4c364601482634033f1537f688544ada
Patrick Morrison <patrick.morrison1498@gmail.com>
2022-04-09 18:45:35 +0800

    add admin siteweather importexport

--- weather/admin.py
+++ weather/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import *
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 
 # Register your models here.
 
-admin.site.register(SiteWeather)
+class SiteWeatherResource(resources.ModelResource):
+
+    class Meta:
+        model = SiteWeather
+
+class SiteWeatherAdmin(ImportExportModelAdmin):
+    resource_class = SiteWeatherResource
+
+admin.site.register(SiteWeather, SiteWeatherAdmin)
