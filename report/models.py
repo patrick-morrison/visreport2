64513faf8eb172296d0a9fa06b8fe6c96005feea
Patrick Morrison <patrick.morrison1498@gmail.com>
2022-04-10 21:50:27 +0800

    increase comment limit

--- report/models.py
+++ report/models.py
@@ -72,7 +72,7 @@ class Report(models.Model):
             MinValueValidator(0)
         ])
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    comments = models.CharField(max_length=30, null=True, blank=True)
+    comments = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return str(self.date.strftime("%Y-%m-%d-%H")) +"_"+ self.site.slug + "_by_"+ self.user.username
