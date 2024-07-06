a1d739013b0f575af63fe6fe02b6ac959278f536
Patrick Morrison <patrick.morrison1498@gmail.com>
2024-07-06 16:42:35 +0800

    fix redirects

--- visreport2/urls.py
+++ visreport2/urls.py
@@ -14,22 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path, register_converter
-from django.views.generic.base import RedirectView
+from django.urls import include, path
 from report import views
 from weather import views as weather_views
-
-class UpperCaseSlugConverter:
-    regex = '[A-Z0-9-]+'
-
-    def to_python(self, value):
-        return value.upper()
-
-    def to_url(self, value):
-        return value.upper()
-
-# Register the converter
-register_converter(UpperCaseSlugConverter, 'upper_slug')
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -37,21 +25,21 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('list', views.list_reports.as_view(), name='list'),
     path('guide', views.guide.as_view(), name='guide'),
-    path('<upper_slug:slug>/', views.detail_site.as_view(), name='detail_site'),
+    path('<slug:slug>', views.detail_site.as_view(), name='detail_site'),
     path('sites.json', views.sites_json.as_view(), name='sites_json'),
     path('reports.json', views.reports_json, name='reports_json'),
-    path('reports/delete/<int:pk>/', views.delete_report.as_view(), name='delete_report'),
+    path('reports/delete/<int:pk>', views.delete_report.as_view(), name='delete_report'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('sites_display.geojson', views.sites_display_geojson.as_view(), name='sites_display_geojson'),
-    path('sites_weather/<upper_slug:slug>/', weather_views.site_weather_display.as_view(), name='siteweatherdisplay'),
-    path('sites_weather/<upper_slug:slug>/edit/', weather_views.site_weather_edit.as_view(), name='siteweatheredit'),
-    path('sites_weather/<upper_slug:slug>/wind_csv/', weather_views.wind_csv, name='wind_csv'),
-    path('sites_weather/<upper_slug:slug>/weather_csv/', weather_views.weather_csv, name='weather_csv'),
+    path('sites_weather/<slug:slug>', weather_views.site_weather_display.as_view(), name='siteweatherdisplay'),
+    path('sites_weather/<slug:slug>/edit/', weather_views.site_weather_edit.as_view(), name='siteweatheredit'),
+    path('sites_weather/<slug:slug>/wind_csv/', weather_views.wind_csv, name='wind_csv'),
+    path('sites_weather/<slug:slug>/weather_csv/', weather_views.weather_csv, name='weather_csv'),
     # AUTH
-    path('accounts/signup/', views.SignUp.as_view(), name='signup'),
+    path('accounts/signup', views.SignUp.as_view(), name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('account/', views.account, name='account'),
-    # Redirects
+    # Custom redirect for sealrocks
     path('sealrocks', RedirectView.as_view(url='/SLR', permanent=True)),
 ]
 
